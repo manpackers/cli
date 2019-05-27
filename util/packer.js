e4d375c4439b8eef5abce78f90c6501010c74951
wangqingzhu <wangqingzhu@lanjingren.com>
2019-05-27 19:21:10 +0800

    modify config content

--- util/packer.js
+++ util/packer.js
@@ -1,12 +1,17 @@
 const path = require('path')
+const fs = require('fs')
 
 const spawn = require('cross-spawn')
 const request = require('request')
+const rimraf = require('rimraf')
 
-const clone = url => spawn.sync('git', ['clone', url], { stdio: 'inherit' })
+const clone = (uri, name) => {
+  spawn.sync('git', ['clone', uri, name])
+  rimraf.sync(path.resolve(name, '.git'))
+}
 
-const install = (url, dirName) => spawn.sync('npm', [
-  'i', '--prefix', path.resolve(dirName), '-f'
+const install = name => spawn.sync('npm', [
+  'i', '--prefix', path.resolve(name), '-f'
 ], { stdio: 'inherit' })
 
 const version = async(url) => await new Promise(resolve => request(
@@ -22,4 +27,5 @@ const version = async(url) => await new Promise(resolve => request(
   }
 ))
 
-module.exports = { clone, install, version }
+const write = (file, str) => fs.writeFileSync(path.resolve(file), str)
+module.exports = { clone, install, version, write }
